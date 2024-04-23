012d458ac07251f9a79036d06145ab79b09d17d5
pgm-justdesc4 <justin.descan@student.arteveldehs.be>
2024-04-23 10:44:47 +0200

    Added college countrybeers demo - week 2

---
+++ week_02/demos/countrybeers/src/components/Beers.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import Beer from "./Beer";
+import SelectCountry from "./SelectCountry";
+
+export default function Beers() {
+  const [country, setCountry] = useState("italy");
+  const [beers, setBeers] = useState([]);
+  const countries = [
+    {
+      name: "Italy",
+      value: "italy",
+    },
+    {
+      name: "Belgium",
+      value: "belgium",
+    },
+    {
+      name: "Sweden",
+      value: "sweden",
+    },
+    {
+      name: "Norway",
+      value: "norway",
+    },
+  ];
+
+  useEffect(() => {
+    const url = `https://beers-list.p.rapidapi.com/beers/${country}`;
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": process.env.REACT_APP_API_BEER_KEY,
+        "X-RapidAPI-Host": "beers-list.p.rapidapi.com",
+      },
+    };
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => setBeers(data));
+  }, [country]);
+
+  return (
+    <div>
+      <SelectCountry
+        countries={countries}
+        setCountry={setCountry}
+        country={country}
+      />
+      <h1>{country}</h1>
+
+      {beers.map((beer, i) => (
+        <Beer key={i} beer={beer} />
+      ))}
+    </div>
+  );
+}
