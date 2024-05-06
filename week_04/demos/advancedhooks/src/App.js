699589bf2261a2e55d71cac30ffb5ef73abeb879
pgm-justdesc4 <justin.descan@student.arteveldehs.be>
2024-05-06 10:50:20 +0200

    Added college Advanced hooks - week 4

---
+++ week_04/demos/advancedhooks/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { useFetch } from "./hooks/useFetch";
+import { ThemeArea } from "./context/ThemeContext";
+import ThemeSwitcher from "./components/ThemeSwitcher";
+
+const url = "https://fakestoreapi.com/products";
+function App() {
+  const { data: products, isLoading, isError } = useFetch(url);
+  if (isLoading) return <>...Loading</>;
+  if (isError) return <>...Error</>;
+  console.log(products);
+  return (
+    <ThemeArea>
+      <ThemeSwitcher />
+      {products.map((product) => (
+        <div key={product.id}>{product.title}</div>
+      ))}
+    </ThemeArea>
+  );
+}
+
+export default App;
